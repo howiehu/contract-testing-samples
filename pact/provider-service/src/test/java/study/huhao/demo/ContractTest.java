36709a96cd2f457e1e0090cc9e96f1297c497fd6
hhu <hhu@thoughtworks.com>
2019-08-19 18:17:13 +0800

    [pact] improve test by mock

--- pact/provider-service/src/test/java/study/huhao/demo/ContractTest.java
+++ pact/provider-service/src/test/java/study/huhao/demo/ContractTest.java
@@ -10,18 +10,32 @@ import au.com.dius.pact.provider.spring.target.SpringBootHttpTarget;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRestPactRunner.class)
 @Provider("provider-service")
 @PactBroker(host = "${pactbroker.host:localhost}", port = "${pactbroker.port:8089}")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ContractTest {
+
+    // We can also use MockMvc to speed up the test's speed to unit test level.
+    // https://github.com/DiUS/pact-jvm/blob/master/provider/pact-jvm-provider-spring/README.md#example-of-mockmvc-test
+
+    @MockBean
+    private UserRepository userRepository;
+
     @TestTarget
     public final Target target = new SpringBootHttpTarget();
 
     @Test
     @State("an user with id 1 exists")
     public void userExists() {
-
+        reset(userRepository);
+        when(userRepository.getById(1)).thenReturn(Optional.of(User.builder().id(1).name("Alex").age(10).build()));
     }
 }
